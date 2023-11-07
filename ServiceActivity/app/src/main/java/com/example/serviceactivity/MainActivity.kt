eda1379e6cab9682ddd69282aa40611d623ae38b
ArkaitzL <127892239+ArkaitzL@users.noreply.github.com>
2023-11-07 12:13:07 +0100

    play

--- ServiceActivity/app/src/main/java/com/example/serviceactivity/MainActivity.kt
+++ ServiceActivity/app/src/main/java/com/example/serviceactivity/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.serviceactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Boton de inicio
+        findViewById<Button>(R.id.play).setOnClickListener{
+            val minutos = findViewById<EditText>(R.id.minutos).text.toString().toInt()
+            val segundos = findViewById<EditText>(R.id.segundos).text.toString().toInt()
+
+            val serviceIntent = Intent(this, countdownService::class.java)
+            serviceIntent.putExtra(countdownService.TIME_EXTRA, (minutos * 60 + segundos) * 1000L)
+
+            // Iniciar el servicio
+            startService(serviceIntent)
+        }
+        findViewById<Button>(R.id.pause).setOnClickListener{
+
+        }
+        findViewById<Button>(R.id.restart).setOnClickListener{
+
+        }
+    }
+
+    fun Finalizar(){
+
     }
 }
--- ServiceActivity/app/src/main/java/com/example/serviceactivity/countdownService.kt
+++ ServiceActivity/app/src/main/java/com/example/serviceactivity/countdownService.kt
@@ -7,6 +7,7 @@ import android.os.IBinder
 import android.util.Log
 import kotlinx.coroutines.*
 
+
 class countdownService : Service() {
 
     private var mediaPlayer: MediaPlayer? = null
