65118ddfc383902c905c31d1fb5680a96702762b
Maider Sanchez <maidersanchezcasas@gmail.com>
2023-11-07 10:55:37 +0100

    finished service

--- ServiceActivity/app/src/main/java/com/example/serviceactivity/countdownService.kt
+++ ServiceActivity/app/src/main/java/com/example/serviceactivity/countdownService.kt
@@ -3,39 +3,38 @@ package com.example.serviceactivity
 import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
-import android.os.CountDownTimer
 import android.os.IBinder
 import android.util.Log
+import kotlinx.coroutines.*
 
 class countdownService : Service() {
 
     private var mediaPlayer: MediaPlayer? = null
-    private var countDownTimer: CountDownTimer? = null
+    private var job: Job? = null
     private var secondsRemaining: Long = 0
+    private var isPaused = false
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val timeInMillis = intent?.getLongExtra(TIME_EXTRA, 0) ?: 0
         secondsRemaining = timeInMillis / 1000
 
-        countDownTimer = object : CountDownTimer(timeInMillis, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                secondsRemaining = millisUntilFinished / 1000
-                Log.d("MyService", "Seconds remaining: $secondsRemaining")
+        job = CoroutineScope(Dispatchers.Main).launch {
+            while (secondsRemaining > 0) {
+                if (!isPaused){
+                    delay(1000)
+                    secondsRemaining--
+                    Log.d("CountdownService", "Seconds remaining: $secondsRemaining")
+                }
             }
-
-            override fun onFinish() {
-                Log.d("MyService", "Countdown finished")
-                playSound()
-                stopSelf()
-            }
+            Log.d("CountdownService", "Countdown finished")
+            playSound()
+            stopSelf()
         }
-
-        countDownTimer?.start()
         return START_STICKY
     }
 
     private fun playSound() {
-        mediaPlayer = MediaPlayer.create(this, R.raw.sound) // Reemplaza con tu archivo de sonido
+        mediaPlayer = MediaPlayer.create(this, R.raw.sound)
         mediaPlayer?.start()
         mediaPlayer?.setOnCompletionListener {
             it.release()
@@ -46,8 +45,16 @@ class countdownService : Service() {
         return null
     }
 
+    fun pauseCountdown() {
+        isPaused = true
+    }
+    fun restartCoutdown(){
+        isPaused = false
+    }
+
+
     override fun onDestroy() {
-        countDownTimer?.cancel()
+        job?.cancel()
         mediaPlayer?.release()
         super.onDestroy()
     }
