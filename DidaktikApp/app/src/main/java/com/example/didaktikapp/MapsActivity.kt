8914a0cbd81641543f1cadcce4d2eebf06a2d7af
ibairuiz1984 <71832421+ibairuiz1984@users.noreply.github.com>
2023-11-20 13:58:06 +0100

    mapas 02

--- DidaktikApp/app/src/main/java/com/example/didaktikapp/MapsActivity.kt
+++ DidaktikApp/app/src/main/java/com/example/didaktikapp/MapsActivity.kt
@@ -1,48 +1,106 @@
 package com.example.didaktikapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.didaktikapp.databinding.ActivityMapsBinding
+import com.example.didaktikapp.mapFragment.PlaceDetailsFragment
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapClickListener,
+    GoogleMap.OnMarkerClickListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // Add markers and move the camera
+        val lezamaUdala = LatLng(43.27424563392292, -2.832868930631767)
+        val santaMaria = LatLng(43.27386287112582, -2.8349717824168814)
+        val frontoia = LatLng(43.273644148421766, -2.833196160210576)
+        val zoomLevel = 15.0f
+        mMap.addMarker(
+            MarkerOptions().position(lezamaUdala).title("Lezamako Udala")
+                .snippet("Texto personalizado para Lezamako Udala")
+        )
+        mMap.addMarker(
+            MarkerOptions().position(santaMaria).title("Santa Maria")
+                .snippet("Texto personalizado para Santa Maria")
+        )
+        mMap.addMarker(
+            MarkerOptions().position(frontoia).title("Frontoia")
+                .snippet("Texto personalizado para Frontoia")
+        )
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lezamaUdala, zoomLevel))
+
+        mMap.setOnMapClickListener(this)
+        mMap.setOnMarkerClickListener(this)
+    }
+
+    override fun onMapClick(point: LatLng) {
+        // Este método se ejecuta cuando se hace clic en cualquier parte del mapa
+        // Puedes realizar acciones adicionales aquí si es necesario
+        Log.d("MapClick", "Click en el mapa: $point")
+    }
+
+    override fun onMarkerClick(marker: Marker): Boolean {
+        // Se ejecuta cuando se hace clic en un marcador
+        // Aquí puedes realizar acciones específicas para el marcador clicado
+        Log.d("MarkerClick", "Click en el marcador: ${marker.title}, Snippet: ${marker.snippet}")
+
+        if (isPredefinedMarker(marker.title)) {
+            openPlaceDetailsFragment(marker)
+        }
+
+        // Devuelve 'true' para indicar que el evento ha sido consumido
+        return true
+    }
+
+    private fun openPlaceDetailsFragment(marker: Marker) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+
+        val bundle = Bundle()
+        bundle.putString("placeName", marker.title)
+        bundle.putString("placeSnippet", marker.snippet)
+
+        // Asigna una URL de imagen específica para cada marcador
+        val imageUrl: String = when (marker.title) {
+            "Lezamako Udala" -> "https://upload.wikimedia.org/wikipedia/commons/b/b3/Lezama_-_Ayuntamiento.jpg"
+            "Santa Maria" -> "https://lh5.googleusercontent.com/p/AF1QipP1Yfn3TJMz8nDxUEs4KpmttBt_auqHEiZJlKH_=w426-h240-k-no"
+            "Frontoia" -> "https://lh5.googleusercontent.com/p/AF1QipNkRuDv1lV45xQHb4itWcv99mNX-s71rMJZaukw=w408-h306-k-no"
+            else -> "https://ejemplo.com/imagen_predeterminada.jpg"
+            // Puedes usar una imagen predeterminada si no hay coincidencia
+        }
+
+        val fragment = PlaceDetailsFragment()
+        fragment.arguments = bundle
+
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.addToBackStack(null)
+        fragmentTransaction.commit()
+    }
+
+    private fun isPredefinedMarker(placeName: String?): Boolean {
+        return placeName == "Lezamako Udala" || placeName == "Santa Maria" || placeName == "Frontoia"
     }
 }
---
+++ DidaktikApp/app/src/main/java/com/example/didaktikapp/mapFragment/PlaceDetailsFragment.kt
@@ -0,0 +1,49 @@
+package com.example.didaktikapp.mapFragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.squareup.picasso.Picasso
+import com.example.didaktikapp.R
+
+class PlaceDetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_place_details, container, false)
+
+        val textViewPlaceName: TextView = rootView.findViewById(R.id.textViewPlaceName)
+        val textViewInfo: TextView = rootView.findViewById(R.id.textViewInfo)
+        val imageViewPlace: ImageView = rootView.findViewById(R.id.imageViewPlace)
+
+        // Recibe información del lugar desde el fragmento, por ejemplo, usando argumentos
+        val args = arguments
+        if (args != null) {
+            val placeName = args.getString("placeName")
+            val placeSnippet = args.getString("placeSnippet")
+            val imageUrl = args.getString("imageUrl")
+
+            // Actualiza las vistas con la información del lugar
+            textViewPlaceName.text = placeName
+            textViewInfo.text = placeSnippet
+
+
+            // Carga la imagen desde la carpeta drawable
+            val imageResId = when (placeName) {
+                "Lezamako Udala" -> R.drawable.udala
+                "Santa Maria" -> R.drawable.santamaria
+                "Frontoia" -> R.drawable.frontoia
+                else -> R.drawable.default_image
+            }
+            imageViewPlace.setImageResource(imageResId)
+        }
+
+        return rootView
+    }
+}
