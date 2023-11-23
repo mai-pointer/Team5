04fac8e23a39626c335e3fd9bc3c858a8a4a2700
ibairuiz1984 <71832421+ibairuiz1984@users.noreply.github.com>
2023-11-23 08:54:22 +0100

    topmaps

--- DidaktikApp/app/src/main/java/com/example/didaktikapp/ToMapsActivity.kt
+++ DidaktikApp/app/src/main/java/com/example/didaktikapp/ToMapsActivity.kt
@@ -5,10 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class ToMapsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_tomaps)
 
         val btnGoToMaps = findViewById<Button>(R.id.button)
 
