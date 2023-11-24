b5242ee9af8edc42ffd0d39a20e72313ab26dc77
Maider Sanchez <maidersanchezcasas@gmail.com>
2023-11-24 12:37:43 +0100

    wip

---
+++ DidaktikApp/app/src/main/java/com/example/didaktikapp/MultipleChoiceActivity.kt
@@ -0,0 +1,12 @@
+package com.example.didaktikapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MultipleChoiceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
