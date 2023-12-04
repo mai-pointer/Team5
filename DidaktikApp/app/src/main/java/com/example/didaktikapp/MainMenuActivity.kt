5013d7d952baeab0c185f8eba134918f136a6d6d
ibairuiz1984 <71832421+ibairuiz1984@users.noreply.github.com>
2023-12-04 10:21:25 +0100

    boton MainMenu jugar

--- DidaktikApp/app/src/main/java/com/example/didaktikapp/MainMenuActivity.kt
+++ DidaktikApp/app/src/main/java/com/example/didaktikapp/MainMenuActivity.kt
@@ -1,11 +1,25 @@
 package com.example.didaktikapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainMenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+        val buttonJugar: Button = findViewById(R.id.button)
+
+        // Agrega un OnClickListener al botón "Jugar"
+        buttonJugar.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                // Crea un Intent para lanzar la actividad MapsActivity
+                val intent = Intent(this@MainMenuActivity, MapsActivity::class.java)
+                startActivity(intent)
+            }
+        })
     }
 }
