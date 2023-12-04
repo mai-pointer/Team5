7be594df22dcbc655d0ba166290f5393b0c585ef
Maider Sanchez <maidersanchezcasas@gmail.com>
2023-12-04 12:45:06 +0100

    wip sopa de letras

--- DidaktikApp/app/src/main/java/com/example/didaktikapp/WordSearchActivity.kt
+++ DidaktikApp/app/src/main/java/com/example/didaktikapp/WordSearchActivity.kt
@@ -1,38 +1,160 @@
 package com.example.didaktikapp
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewTreeObserver
+import android.widget.FrameLayout
 import android.widget.TableLayout
 import android.widget.TableRow
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import kotlinx.coroutines.*
+import kotlin.random.Random
 
 class WordSearchActivity: AppCompatActivity() {
     private lateinit var tableLayout: TableLayout
     private lateinit var myWordSearch: Array<Array<Char>>
-
     val size = 8
+    private val wordList = listOf("ODOLA", "KIPULA", "PORRUA", "HESTEA", "KOIPEA")
+
+    val wordMap: Map<String, MutableList<Pair<Int, Int>>> = mapOf(
+        "ODOLA" to mutableListOf(),
+        "KIPULA" to mutableListOf(),
+        "PORRUA" to mutableListOf(),
+        "HESTEA" to mutableListOf(),
+        "KOIPEA" to mutableListOf()
+    )
+
 
     private var startCoordinate: Pair<Float, Float>? = null
+    private var newWord:String = ""
+    private var selectedCells: MutableList<Int> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_word_search)
 
-        tableLayout = findViewById(R.id.Hitzak)
+        tableLayout = findViewById(R.id.wordSearch)
         val layoutParams = tableLayout.layoutParams
         layoutParams.height = layoutParams.width
         tableLayout.layoutParams = layoutParams
 
-        CreateGrid(tableLayout)
         myWordSearch = Array(size) { Array(size) { ' ' } }
+        CreateWordSearch()
+        CreateGrid(tableLayout)
+
 
     }
+
+    fun CreateWordSearch(){
+        SetFirstWord()
+        wordMap.keys.forEachIndexed { index, element ->
+            if (index > 0) {
+                element
+            }
+        }
+
+    }
+
+    fun HasCommonChar(myCurrentWord: String, previousWord:String):Pair<Int,Int>?{
+        for ((indexChar, char) in myCurrentWord.withIndex()) {
+            for ((indexPreChar, preChar) in previousWord.withIndex()) {
+                if (char == preChar) {
+                    return Pair(indexChar, indexPreChar)
+                }
+            }
+        }
+        return null
+    }
+
+    fun SetFirstWord(){
+        val numRows = myWordSearch.size
+        val numCols = myWordSearch[0].size
+
+        val randomRow = Random.nextInt(numRows)
+        val randomCol = Random.nextInt(numCols)
+        var randomDirection = Random.nextInt(Direction.values().size)
+        //val randomChar = myWordSearch[randomRow][randomCol]
+
+        while (!canWriteWord(randomRow, randomCol, wordList[0], Direction.values()[randomDirection])) {
+            randomDirection = (randomDirection + 1) % Direction.values().size
+        }
+        writeWord(randomRow, randomCol, wordMap.keys.elementAt(0), Direction.values()[randomDirection])
+    }
+    enum class Direction {
+        HORIZONTAL,
+        REVERSE_HORIZONTAL,
+        VERTICAL_UP,
+        VERTICAL_DOWN,
+        DIAGONAL_UP_RIGHT,
+        DIAGONAL_UP_LEFT,
+        DIAGONAL_DOWN_RIGHT,
+        DIAGONAL_DOWN_LEFT
+    }
+    private fun canWriteWord(startRow: Int, startCol: Int, word: String, direction:Direction):Boolean{
+        when (direction) {
+            Direction.HORIZONTAL -> if (startCol + word.length > size) return false
+            Direction.REVERSE_HORIZONTAL -> if (startCol - word.length < 0) return false
+            Direction.VERTICAL_DOWN -> if (startRow + word.length > size) return false
+            Direction.VERTICAL_UP -> if (startRow - word.length < 0) return false
+            Direction.DIAGONAL_UP_RIGHT -> if (startRow - word.length < 0 || startCol + word.length > size) return false
+            Direction.DIAGONAL_UP_LEFT -> if (startRow - word.length < 0 || startCol - word.length < 0) return false
+            Direction.DIAGONAL_DOWN_RIGHT -> if (startRow + word.length > size || startCol + word.length > size) return false
+            Direction.DIAGONAL_DOWN_LEFT -> if (startRow + word.length > size || startCol - word.length < 0) return false
+        }
+        return true
+    }
+
+    fun writeWord(startRow: Int, startCol: Int, word: String, direction: Direction) {
+        val coordinates = mutableListOf<Pair<Int, Int>>()
+
+        for (i in word.indices) {
+            when (direction) {
+                Direction.HORIZONTAL -> {
+                    myWordSearch[startRow][startCol + i] = word[i]
+                    coordinates.add(Pair(startRow, startCol + i))
+
+                }
+                Direction.REVERSE_HORIZONTAL -> {
+                    myWordSearch[startRow][startCol - i] = word[i]
+                    coordinates.add(Pair(startRow, startCol - i))
+
+                }
+                Direction.VERTICAL_DOWN -> {
+                    myWordSearch[startRow + i][startCol] = word[i]
+                    coordinates.add(Pair(startRow + i, startCol))
+
+                }
+                Direction.VERTICAL_UP -> {
+                    myWordSearch[startRow - i][startCol] = word[i]
+                    coordinates.add(Pair(startRow - i, startCol))
+                }
+                Direction.DIAGONAL_UP_RIGHT -> {
+                    myWordSearch[startRow - i][startCol + i] = word[i]
+                    coordinates.add(Pair(startRow - i, startCol + i))
+                }
+                Direction.DIAGONAL_UP_LEFT -> {
+                    myWordSearch[startRow - i][startCol - i] = word[i]
+                    coordinates.add(Pair(startRow - i, startCol - i))
+                }
+                Direction.DIAGONAL_DOWN_RIGHT -> {
+                    myWordSearch[startRow + i][startCol + i] = word[i]
+                    coordinates.add(Pair(startRow + i, startCol + i))
+                }
+                Direction.DIAGONAL_DOWN_LEFT -> {
+                    myWordSearch[startRow + i][startCol - i] = word[i]
+                    coordinates.add(Pair(startRow + i, startCol - i))
+                }
+            }
+        }
+        wordMap[word]?.addAll(coordinates)
+    }
+
+
 
     fun CreateGrid(tableLayout:TableLayout) {
         tableLayout.viewTreeObserver.addOnGlobalLayoutListener(
@@ -48,16 +170,26 @@ class WordSearchActivity: AppCompatActivity() {
                         val tableRow = TableRow(this@WordSearchActivity)
 
                         for (j in 0 until size) {
-                            val textView = TextView(this@WordSearchActivity)
+                            val frameLayout = FrameLayout(this@WordSearchActivity)
 
                             val layoutParams = TableRow.LayoutParams(cellSize, cellSize)
-                            textView.layoutParams = layoutParams
+                            frameLayout.layoutParams = layoutParams
+                            frameLayout.setPadding(15,15,15,15)
+                            // Crear un TextView dentro del FrameLayout
+                            val textView = TextView(this@WordSearchActivity)
 
-                            textView.gravity = Gravity.CENTER
-                            textView.text = "A"
-                            textView.setBackgroundColor(
-                                resources.getColor(R.color.blancoOscuro, null)
+                            val textLayoutParams = FrameLayout.LayoutParams(
+                                FrameLayout.LayoutParams.MATCH_PARENT,
+                                FrameLayout.LayoutParams.MATCH_PARENT
                             )
+                            textView.layoutParams = textLayoutParams
+                            textView.gravity = Gravity.CENTER
+                            if(!myWordSearch[i][j].toString().equals("")){
+                                textView.text = myWordSearch[i][j].toString()
+                            } else{
+                                textView.text = ""
+                            }
+                            //textView.text = "A"
                             val uniqueId = "R:$i C:$j"
                             textView.id = uniqueId.hashCode()
 
@@ -65,7 +197,8 @@ class WordSearchActivity: AppCompatActivity() {
                                 onTouchEvent(event)
                             }
 
-                            tableRow.addView(textView)
+                            frameLayout.addView(textView)
+                            tableRow.addView(frameLayout)
                         }
                         tableLayout.addView(tableRow)
                     }
@@ -75,6 +208,7 @@ class WordSearchActivity: AppCompatActivity() {
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
+
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
                 startCoordinate = Pair(event.rawX, event.rawY)
@@ -85,9 +219,15 @@ class WordSearchActivity: AppCompatActivity() {
                     for (i in 0 until tableLayout.childCount) {
                         val row = tableLayout.getChildAt(i) as TableRow
                         for (j in 0 until row.childCount) {
-                            val textView = row.getChildAt(j) as TextView
+                            val frameLayout = row.getChildAt(j) as FrameLayout
+                            val textView = frameLayout.getChildAt(0) as TextView
+
                             if (isPointInsideView(startCoord, textView, event.rawX, event.rawY)) {
-                                textView.setBackgroundColor(ContextCompat.getColor(this@WordSearchActivity, R.color.verdeClaro))
+                                if(!selectedCells.contains(textView.id)){
+                                    newWord += textView.text
+                                    selectedCells.add(textView.id)
+                                    frameLayout.setBackgroundColor(ContextCompat.getColor(this@WordSearchActivity, R.color.verdeClaro))
+                                }
                             }
                         }
                     }
@@ -96,18 +236,36 @@ class WordSearchActivity: AppCompatActivity() {
             }
             MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
                 startCoordinate = null
-                handleClick()
+                HandleClick()
                 return true
             }
             else -> return false
         }
     }
 
-    private fun handleClick() {
-        // Implementa aquí la lógica de manejo de clics
-        // Por ejemplo, puedes realizar las acciones que necesitas al hacer clic
-    }
+    private fun HandleClick(){
+        val toast = Toast.makeText(this, newWord, Toast.LENGTH_LONG)
+        toast.show()
 
+        for (i in 0 until tableLayout.childCount) {
+            val row = tableLayout.getChildAt(i) as TableRow
+            for (j in 0 until row.childCount) {
+                val frameLayout = row.getChildAt(j) as FrameLayout
+                val textView = frameLayout.getChildAt(0) as TextView
+
+                selectedCells.forEach{code ->
+                    if (code.equals(textView.id)){
+                        frameLayout.setBackgroundColor(ContextCompat.getColor(this@WordSearchActivity, R.color.blancoOscuro))
+                    }
+
+                }
+            }
+
+        }
+
+        newWord = ""
+        selectedCells.clear()
+    }
 
     private fun isPointInsideView(startCoord: Pair<Float, Float>, view: View, x: Float, y: Float): Boolean {
         val location = IntArray(2)
