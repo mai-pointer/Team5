17804ab4894142f5657f41fe16572ceaea3fbc67
ArkaitzL <127892239+ArkaitzL@users.noreply.github.com>
2023-12-11 08:44:07 +0100

    crucigrama

--- DidaktikApp/app/build.gradle.kts
+++ DidaktikApp/app/build.gradle.kts
@@ -44,6 +44,8 @@ android {
 dependencies {
     implementation("com.google.android.material:material:1.6.0")
     implementation("com.makeramen:roundedimageview:2.3.0")
+
+    implementation ("androidx.gridlayout:gridlayout:1.0.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
@@ -52,6 +54,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
     implementation ("androidx.databinding:databinding-runtime:7.0.0")
+    implementation("androidx.gridlayout:gridlayout:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
