7d3bd2181e18209ff9e9642d0887fad576c257d5
ArkaitzL <127892239+ArkaitzL@users.noreply.github.com>
2023-12-11 08:35:26 +0100

    Crucigrama terminado

--- DidaktikApp/app/build.gradle.kts
+++ DidaktikApp/app/build.gradle.kts
@@ -43,6 +43,7 @@ android {
 
 dependencies {
 
+    implementation ("androidx.gridlayout:gridlayout:1.0.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
--- DidaktikApp/app/src/main/java/com/example/didaktikapp/Crucigrama.kt
+++ DidaktikApp/app/src/main/java/com/example/didaktikapp/Crucigrama.kt
@@ -1,101 +1,275 @@
 package com.example.didaktikapp
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
+import android.text.Editable
 import android.text.InputFilter
 import android.text.InputType
-import android.util.Log
+import android.text.TextWatcher
 import android.view.Gravity
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import android.widget.GridLayout
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class Crucigrama : AppCompatActivity() {
 
-    val gridX = 10
-    val gridY = 10
-    val palabras: List<Palabra> = listOf(
-        Palabra(this, "COCHE", "Medio de transporte", 0, 1, true),
-        Palabra(this, "COCHE", "Medio de transporte", 1, 1, true),
-        Palabra(this, "COCHE", "Medio de transporte", 2, 1, true),
-        Palabra(this, "COCHE", "Medio de transporte", 3, 1, true),
-        Palabra(this, "COCHE", "Medio de transporte", 4, 1, true),
-        Palabra(this, "COCHE", "Medio de transporte", 5, 1, true),
-        )
+    //VARIABLES PARA LA PLANTILLA Y LAS PISTAS
+    val pistas = arrayListOf(
+        Pista(this, "Zerbait handiagoa egin"),
+        Pista(this, "Athletic Clubaren zelaiaren izena"),
+        Pista(this, "Atzean daukazuena, futbol..."),
+        Pista(this, "San Mamesetik mugitu zena eta orain hemen dagoena"),
+        Pista(this, "Arkua dagoen herriaren izena"),
+    )
+    val plantilla = """
+            [       |         |       |         |       |       | H , 1   ]
+            [       |         |       |         |       |       | A , 1   ]
+            [       | S , 2   |       |         |       |       | N , 1   ]
+            [       | A , 2   |       | Z , 3   |       |       | D , 1   ]
+            [       | N , 2   |       | E , 3   |       |       | I , 1   ]
+            [       | M , 2   |       | L , 3   |       |       | T , 1   ]
+            [       | A , 2   |       | A , 4,3 | R , 4 | K , 4 | U , 1,4 ]
+            [       | M , 2   |       | I , 3   |       |       |         ]
+            [ L , 5 | E , 5,2 | Z , 5 | A , 3,5 | M , 5 | A , 5 |         ]
+            [       | S , 2   |       |         |       |       |         ]
+        """
+    //OTRAS
+    var pista_actual = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crucigrama)
 
-        //Crea el array de celdas
-        val celdas = Array(gridX) {
-            Array(gridY) {
-                Celda()
-            }
-        }
+        //VARIABLES
+        val crucigrama: List<List<Celda>> = Crear(plantilla.trimIndent())
+        val grid = findViewById<androidx.gridlayout.widget.GridLayout>(R.id.crucigrama)
 
-        //Comprueba si alguna palabra tiene una letra en esa posición
-        for ((indice, palabra) in palabras.withIndex()) {
-            //Añade la palabra a la celda
-            for (elemento in palabra.elementos) {
-                celdas[elemento.x][elemento.y].caracter = elemento.caracter
-                celdas[elemento.x][elemento.y].posicion_palabra = indice
-            }
 
-            //AVISA SI SALE DEL GRID O SI SE JUNTAN DOS CARACTERES QUE NO SON IGUALES***
-        }
+        // CREAR EL CRUCIGRAMA
+        for ((filaIndex, fila) in crucigrama.withIndex()) {
+            val params = GridLayout.LayoutParams()
+            params.rowSpec = GridLayout.spec(filaIndex)
 
-        //Crea el grid layout
-        val crucigrama = findViewById<GridLayout>(R.id.crucigrama)
+            // Ajustar el número de columnas en el GridLayout para esta fila
+            grid.setColumnCount(fila.size)
 
-        for (i in celdas.indices) {
-            for (j in celdas[i].indices) {
-                val celda = celdas[i][j]
-                Log.i("CRUCIGRAMA", "Celda $i $j: ${celda.caracter}")
-                ///EN CADA CELDA AÑADE O UN EDIT TEXT O UN ESPACIO SI EL CARACTER ES UN ESPACIO***
-            }
-        }
-    }
+            for ((columnaIndex, celda) in fila.withIndex()) {
+                // Añadir una vista a la cuadrícula
+                params.columnSpec = GridLayout.spec(columnaIndex)
 
-    //Clase para guardar cada elemento de la celda
-    class Celda{
-        var caracter: Char = ' '
-        var posicion_palabra: Int = 0
-    }
+                val cellSize = resources.getDimensionPixelSize(R.dimen.cell_size)
+                params.setMargins(2, 1, 2, 1)
+                params.width = cellSize
+                params.height = cellSize
+                params.setGravity(Gravity.FILL)
 
-    //Clase que contiene la info de la palabra
-    class Palabra(
-        //Valores de la palabra
-        val context: Context,
-        val palabra: String,
-        val pista: String,
-        val x: Int,
-        val y: Int,
-        val horizontal: Boolean
-    ){
-        //Info de cada letra
-        class Elemento(val context: Context, val x: Int, val y: Int){
-            val caracter = ' '
-        }
-        var editText: List<EditText> = listOf()
-        var elementos: List<Elemento> = listOf()
+                if (celda.caracter != ' ') {
+                    val text = Text1()
+                    text.layoutParams = params
+                    grid.addView(text)
 
-        //Inicializa la info de cada letra
-        init {
-            var cont : Int = 0
-            var elemento = Elemento(context, x, y)
+                    // Crear las pistas
+                    if (celda.pista >= 0){
+                        pistas[celda.pista-1].casillas.add(Pista.Casilla(celda.caracter, text))
+                    }
+                    if (celda.pista_secundaria >= 0){
+                        pistas[celda.pista_secundaria-1].casillas.add(Pista.Casilla(celda.caracter, text))
+                    }
 
-            for (c in palabra.split("")){
-                elemento.caracter.plus(c)
-                if(horizontal){
-                    elemento.x.plus(x + cont)
-                    elemento.y.plus(y)
-                } else{
-                    elemento.x.plus(x)
-                    elemento.y.plus(y + cont)
+                } else {
+                    val text = Text2()
+                    text.layoutParams = params
+                    grid.addView(text)
                 }
-                cont += 1;
             }
         }
+
+        //CREAR LAS PISTAS
+        for ((index, pista) in pistas.withIndex()){
+            pista.Activar(index)
+        }
+
+        Pistas(0)
+
+        findViewById<ImageButton>(R.id.mas_pista).setOnClickListener{
+            Pistas(1)
+        }
+        findViewById<ImageButton>(R.id.menos_pista).setOnClickListener{
+            Pistas(-1)
+        }
+
+        //TERMINAR PARTIDA
+        findViewById<Button>(R.id.terminar_crucigrama).setOnClickListener{
+            //Comprueba si esta bien
+            var bien = true
+            for (pista in pistas){
+                //Corrigiendo cada pista
+                for (casilla in pista.casillas){
+                    if (!casilla.editText.text.toString().isNotBlank()){
+                        bien = false
+                        break;
+                    }
+                    if (casilla.editText.text.toString().uppercase()!= casilla.caracter.toString().uppercase()){
+                        bien = false
+                        Toast.makeText(this, "¡Has fallado!", Toast.LENGTH_SHORT).show()
+                        casilla.editText.setTextColor(Color.RED)
+                    }
+                }
+            }
+            if (bien){
+                Toast.makeText(this, "¡Terminado!", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
+    fun Pistas(cambio: Int, literal: Boolean = false){
+
+        //Cambia el color
+        CambiarColor(Color.BLACK)
+        //Desactiva la pista
+        pistas[pista_actual].activa = false
+
+        // Cambiar la pista actual
+        if (!literal){
+            var nueva = cambio + pista_actual
+            if (nueva < 0) nueva = pistas.size - 1
+            if (nueva >= pistas.size) nueva = 0
+
+            pista_actual = nueva
+        } else {
+            pista_actual = cambio
+        }
+
+        //Cambiar el texto de la pista
+        findViewById<TextView>(R.id.pista).text = pistas[pista_actual].texto
+
+        //Cambia el colors
+        CambiarColor(ContextCompat.getColor(this, R.color.verdeOscuro))
+        //Activa la pista
+        pistas[pista_actual].activa = true
+    }
+
+    fun CambiarColor(color: Int) {
+        //Cambiar el color de la pista
+        for (casilla in pistas[pista_actual].casillas) {
+            val border = GradientDrawable()
+            border.setColor(Color.WHITE)
+            border.setStroke(4, color)
+            border.cornerRadius = 8f
+
+            casilla.editText.background = border
+        }
+    }
+
+    fun Text1(): EditText {
+        val editText = EditText(this)
+        editText.setPadding(1, 1, 1, 1)
+        editText.textSize = 14f
+        editText.gravity = Gravity.CENTER
+        editText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS
+//        editText.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(1))
+
+        // Configurar bordes redondeados y más gruesos
+        val border = GradientDrawable()
+        border.setColor(Color.WHITE) // Fondo blanco
+        border.setStroke(4, Color.BLACK) // Grosor y color del borde
+        border.cornerRadius = 8f // Radio del borde, ajusta según sea necesario
+
+        editText.background = border
+
+        return editText
+    }
+
+    fun Text2(): TextView {
+        val textView = TextView(this)
+        textView.text = " "
+        textView.gravity = Gravity.CENTER
+
+        return textView
+    }
+
+    // Función para crear un crucigrama a partir de una plantilla en formato de cadena
+    fun Crear(plantilla: String): List<List<Celda>> {
+        return plantilla.lines().filter { fila ->                       // Lo divide en lineas
+            fila.isNotBlank() }.map { fila ->                           // Elimina las lineas en blanco
+                fila.trim()
+                    .substring(1, fila.length - 1)                      // Elimina los corchetes al principio y al final
+                    .split("|")                              // Divide la fila en columnas apartir de las comas
+                    .map { columna ->
+                        val elementos = columna.trim().split(',')
+                        val (caracter, pista, pista_secundaria) =
+                            if (elementos.size >= 3) elementos
+                            else if (elementos.size == 2) elementos + listOf("-1")
+                            else listOf("", "-1", "-1")
+                        Celda(caracter.firstOrNull() ?: ' ', pista.trim().toIntOrNull() ?: -1, pista_secundaria.trim().toIntOrNull() ?: -1)
+                    }
+        }
+    }
+
+    data class Celda(val caracter: Char, val pista: Int, val pista_secundaria: Int) {
+        var caracterJugador: Char = ' '
+    }
+
+    data class Pista(val context: Crucigrama ,val texto: String){
+        var casillas = mutableListOf<Casilla>()
+        var activa = false
+        var numero = -1
+
+        fun Activar(numero: Int) {
+            this.numero = numero
+
+            for ((index, casilla) in casillas.withIndex()) {
+
+                //Cuando se le pone el foco al EditText
+                casilla.editText.onFocusChangeListener = View.OnFocusChangeListener { _, hasFocus ->
+                    if (hasFocus) {
+                        // Se activa solo cuando se le da el foco, no cuando se lo quita
+                        if (casilla.editText.tag != "autoFocus") {
+                            context.Pistas(numero, true)
+                        }
+                    } else {
+                        // Aquí es donde puedes quitar el tag una vez que ya no lo usas
+                        casilla.editText.tag = null
+                    }
+                }
+
+                //Cuando se escribe en el EditText
+                casilla.editText.addTextChangedListener(object : TextWatcher {
+                    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) { }
+                    override fun afterTextChanged(s: Editable?) {
+                        //Pone el color original
+                        casilla.editText.setTextColor(Color.BLACK)
+
+                        // Limitar el número de caracteres a 1
+                        if (s?.length ?: 0 > 1) {
+                            casilla.editText.setText(s?.subSequence(0, 1))
+                            casilla.editText.setSelection(1) // Mover el cursor al final del texto
+                        }
+
+                        // Cambiar el foco al siguiente EditText si hay caracteres escritos
+                        if (!s.isNullOrBlank() && activa) {
+                            val nextIndex = index + 1
+                            if (nextIndex < casillas.size) {
+                                //Salta al siguiente EditText
+                                casillas[nextIndex].editText.tag = "autoFocus"
+                                casillas[nextIndex].editText.requestFocus()
+                            }
+                        }
+                    }
+                })
+            }
+        }
+
+        data class Casilla(val caracter: Char, val editText: EditText)
     }
 }
