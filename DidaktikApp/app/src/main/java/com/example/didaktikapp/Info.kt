74980ef0c6b958653627556c02ab3b6ffc3123c1
Arkaitz <127892239+ArkaitzL@users.noreply.github.com>
2024-01-29 19:32:43 +0100

    bug fix

--- DidaktikApp/app/src/main/java/com/example/didaktikapp/Info.kt
+++ DidaktikApp/app/src/main/java/com/example/didaktikapp/Info.kt
@@ -4,6 +4,7 @@ import android.graphics.PorterDuff
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import android.widget.Button
 import android.widget.FrameLayout
