f57361c6faab19f1362b606a6629d6c70755f11a
Maider Sanchez <maidersanchezcasas@gmail.com>
2024-01-30 11:37:01 +0100

    fix

--- DidaktikApp/app/src/main/java/com/example/didaktikapp/MapsActivity.kt
+++ DidaktikApp/app/src/main/java/com/example/didaktikapp/MapsActivity.kt
@@ -47,13 +47,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapCli
 
 
     // Variable para saber si el usuario es admin o no
-    private var esAdmin: Boolean = true
+    private var esAdmin: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         //Inicia el HASIERAKO JARDUERA
-        BDManager.Iniciar{ partidaDao, competitivoDao, sharedPreferences ->
+        /*BDManager.Iniciar{ partidaDao, competitivoDao, sharedPreferences ->
             GlobalScope.launch(Dispatchers.IO){
                 val id = sharedPreferences.getInt("partida_id", 1)
                 val partida = partidaDao.get(id)
@@ -71,12 +71,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapCli
                     )
                 }
             }
-        }
+        }*/
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
         esAdmin = intent.getBooleanExtra("admin", false)
-        progressBar = findViewById(R.id.progressBar)
 
         setupHeaderFragment(savedInstanceState)
 
